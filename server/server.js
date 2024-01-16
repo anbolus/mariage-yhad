9f006b03b95c55c20993dbb0b38c2a6717cd6a77
anbolus <andrea.bolus@epitech.eu>
2024-01-16 14:09:18 +0100

    ajout page admin-login

--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const port = 5000;
 
-/* app.get('/', (req, res) => {
-    res.send("Backend working !");
-}); */
-
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../public'));
+app.set('views', path.join(__dirname, '../public/views'));
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
         res.render('index', {message: 'Hello from the server!'});
     });
 
+app.get('/admin/login', (req, res) => {
+        res.render('admin-login');
+    });
+
+app.post('/admin/login', (req, res) => {
+        const { username, password } = req.body;
+
+        //TODO: implémenter logique d'authentification admin
+
+        res.redirect('/admin/dashboard');
+
+    });
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
