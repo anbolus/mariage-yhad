ba466627f53629e358db8e738942299a85adf9b3
anbolus <andrea.bolus@epitech.eu>
2024-01-16 13:18:05 +0100

    ajout homepage index.ejs

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 5000;
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.send("Backend working !");
-});
+}); */
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../public'));
+
+
+app.get('/', (req, res) => {
+        res.render('index', {message: 'Hello from the server!'});
+    });
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
