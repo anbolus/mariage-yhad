4597505844bbe19aa0af82e0eaa63f1804a68cc3
anbolus <and.bolus@gmail.com>
2024-02-17 15:39:36 +0100

    edit style homepage

--- server/server.js
+++ server/server.js
@@ -8,9 +8,6 @@ const db = require('./db');
 const authRoutes = require('./routes/authRoutes.js');
 const tokenHandler = require('./middleware/tokenHandler.js');
 const authRouter = require('./routes/authRoutes.js');
-const { generateToken } = require('./middleware/tokenHandler.js');
-const requireLogin = require('./middleware/tokenHandler.js');
-
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../public/views'));
@@ -22,6 +19,15 @@ app.use(bodyParser.json());
 app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+/* app.use((req, res, next) => {
+    res.status(404).render('404');
+}) */
+
+/* app.get('/styles/style.css', function(req, res) {
+    res.type('text/css');
+
+}); */
 app.get('/', (req, res) => {
     res.render('index', { message: 'Hello from the server!' });
 });
@@ -30,14 +36,17 @@ app.get('/admin/login', (req, res) => {
     res.render('admin-login');
 });
 
+app.get('/plan-acces', (req, res) => {
+    res.render('plan-acces', { message: 'Plan d\'acces' });
+    })
 app.get('/admin/register', (req, res) => {
     res.render('admin-register');
 })
 
-app.get('/admin/dashboard', tokenHandler.requireLogin, function (req, res) {
+/* app.get('/admin/dashboard', tokenHandler.requireAuth, function (req, res) {
     res.render('admin-dashboard', { username: req.user.username });
 })
-
+ */
 app.use('/admin', authRouter);
 
 app.use('/', authRoutes);
